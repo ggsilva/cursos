a218f8353e32a6ffa8a76365d796402d0d9142c9
ggsilva <gg-silva@outlook.com>
2014-05-30 17:59:37 -0300

    Commit Inicial

---
+++ DesignPatterns/src/br/com/treinaweb/designPatterns/testes/DesignPatterns.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.designPatterns.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DesignPatterns {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
