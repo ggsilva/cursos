62bf92f8dae62a59ee96ab3235b9ab3c2ed4ca7a
ggsilva <gg-silva@outlook.com>
2014-12-19 18:37:19 -0200

    Adicionado cenários que testam as novas formas de filtro de lista e ordenação, utiizando Comparator e Predicate com Lambda.

--- CasaDoCodigo/src/test/java/br/com/java8/LambdaTest.java
+++ CasaDoCodigo/src/test/java/br/com/java8/LambdaTest.java
@@ -1,10 +1,14 @@
 package br.com.java8;
 
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import org.junit.Test;
 
@@ -16,22 +20,18 @@ public class LambdaTest {
 	@Test
 	public void imprimeUsuariosDaLista() {
 		Capitulo2 c = new Capitulo2();
-		c.addUsuario(new Usuario("Guilherme Silva", 20));
-		c.addUsuario(new Usuario("Alana Pereira", 15));
-		assertEquals("[Guilherme Silva, 20], [Alana Pereira, 15]", c.imprimeUsuarios());
+		c.addUsuario(new Usuario("Guilherme Silva", 15));
+		c.addUsuario(new Usuario("Alana Pereira", 20));
+		assertEquals("[Guilherme Silva, 15], [Alana Pereira, 20]", c.imprimeUsuarios());
 	}
 
 	@Test
 	public void imprimeUsuariosUsandoMetodoDefault() {
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		usuarios.add(new Usuario("Guilherme Silva", 20));
-		usuarios.add(new Usuario("Alana Pereira", 15));
-		usuarios.add(new Usuario("Noah Gabriel", 35));
+		List<Usuario> usuarios = getListaDeUsuarios();
 		
 		StringBuilder builder = new StringBuilder();
 		
-		String mensagem = "antes de imprimir os nomes";
-		Consumer<Usuario> mostraMensagem = u -> builder.append(mensagem    + "\n");
+		Consumer<Usuario> mostraMensagem = u -> builder.append("antes de imprimir os nomes" + "\n");
 		Consumer<Usuario> imprimeNomes   = u -> builder.append(u.getNome() + "\n");
 		
 		usuarios.forEach(mostraMensagem.andThen(imprimeNomes));
@@ -42,6 +42,69 @@ public class LambdaTest {
 				    + "Alana Pereira\n"
 				    + "antes de imprimir os nomes\n"
 				    + "Noah Gabriel\n", builder.toString());
+	}
+
+	@Test
+	public void filtraUsuariosUtilizandoPredicate() {
+		List<Usuario> usuarios = getListaDeUsuarios();
+		
+		StringBuilder builder = new StringBuilder();
+		
+		Predicate<Usuario> p = u -> u.getPontos() > 10;
+		usuarios.removeIf(p);
+		usuarios.forEach((Consumer<Usuario>) u -> builder.append(u.getNome() + "\n"));
+		
+		assertEquals( "Noah Gabriel\n", builder.toString());
+	}
+
+	@Test
+	public void ordenandoPorNomeUtilizandoComparatorELambda() {
+		List<Usuario> usuarios = getListaDeUsuarios();
+
+		Comparator<Usuario> comparator = (u1, u2) -> u1.getNome().compareTo(u2.getNome());	
+		usuarios.sort(comparator);
+		
+		StringBuilder builder = new StringBuilder();
+		usuarios.forEach((Consumer<Usuario>) u -> builder.append(u + "\n"));
+		
+		assertEquals( "[Alana Pereira, 20]\n"
+				    + "[Guilherme Silva, 15]\n"
+				    + "[Noah Gabriel, 10]\n", builder.toString());
+	}
+
+	@Test
+	public void ordenandoPorPontosUtilizandoComparatorComMetodoEstaticoELambda() {
+		List<Usuario> usuarios = getListaDeUsuarios();
+
+		Comparator<Usuario> comparator = Comparator.comparing(u -> u.getPontos());	
+		usuarios.sort(comparator);
+		
+		StringBuilder builder = new StringBuilder();
+		usuarios.forEach((Consumer<Usuario>) u -> builder.append(u + "\n"));
+		
+		assertEquals( "[Noah Gabriel, 10]\n"
+			        + "[Guilherme Silva, 15]\n"
+			        + "[Alana Pereira, 20]\n", builder.toString());
+	}
+
+	@Test
+	public void ordenandoUmaListaDeStrings() {
+		List<Usuario> usuarios = getListaDeUsuarios();
+		List<String> nomes = new ArrayList<String>();
+
+		usuarios.forEach(u -> nomes.add(u.getNome()));
+		
+		nomes.sort(naturalOrder());
+		
+		assertEquals( "[Alana Pereira, Guilherme Silva, Noah Gabriel]", nomes.toString());
+	}
+
+	private List<Usuario> getListaDeUsuarios() {
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(new Usuario("Guilherme Silva", 15));
+		usuarios.add(new Usuario("Alana Pereira", 20));
+		usuarios.add(new Usuario("Noah Gabriel", 10));
+		return usuarios;
 	}
 
 }
