d03385d2ad6ac8cfa07b987182cf790ffef7d413
ggsilva <gg-silva@outlook.com>
2014-12-19 17:38:22 -0200

    Adicionado teste do método default da interface consumer.

--- CasaDoCodigo/src/test/java/br/com/java8/LambdaTest.java
+++ CasaDoCodigo/src/test/java/br/com/java8/LambdaTest.java
@@ -2,6 +2,10 @@ package br.com.java8;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 import org.junit.Test;
 
 import br.com.java8.Capitulo2;
@@ -15,6 +19,29 @@ public class LambdaTest {
 		c.addUsuario(new Usuario("Guilherme Silva", 20));
 		c.addUsuario(new Usuario("Alana Pereira", 15));
 		assertEquals("[Guilherme Silva, 20], [Alana Pereira, 15]", c.imprimeUsuarios());
+	}
+
+	@Test
+	public void imprimeUsuariosUsandoMetodoDefault() {
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(new Usuario("Guilherme Silva", 20));
+		usuarios.add(new Usuario("Alana Pereira", 15));
+		usuarios.add(new Usuario("Noah Gabriel", 35));
+		
+		StringBuilder builder = new StringBuilder();
+		
+		String mensagem = "antes de imprimir os nomes";
+		Consumer<Usuario> mostraMensagem = u -> builder.append(mensagem    + "\n");
+		Consumer<Usuario> imprimeNomes   = u -> builder.append(u.getNome() + "\n");
+		
+		usuarios.forEach(mostraMensagem.andThen(imprimeNomes));
+		
+		assertEquals( "antes de imprimir os nomes\n"
+				    + "Guilherme Silva\n"
+				    + "antes de imprimir os nomes\n"
+				    + "Alana Pereira\n"
+				    + "antes de imprimir os nomes\n"
+				    + "Noah Gabriel\n", builder.toString());
 	}
 
 }
